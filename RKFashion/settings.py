c31939833bb554846d2a82e4bb7caccf29c204eb
rahul-hasan25 <rahulhaasan2022@gmail.com>
2025-12-13 13:34:37 +0600

    django authentication

--- RKFashion/settings.py
+++ RKFashion/settings.py
@@ -136,3 +136,18 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Messages in Django
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
+
+
+#SMTP Configeration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'rahulhaasan2022@gmail.com'
+EMAIL_HOST_PASSWORD = 'jlbn dkal ihjg qlsw'
+EMAIL_USE_TLS = True
